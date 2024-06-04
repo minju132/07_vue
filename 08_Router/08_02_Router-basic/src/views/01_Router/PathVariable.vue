542cc77055aa6482d52f866551f5389612367518
jaehee <sally0723s@gmail.com>
2024-06-04 15:31:07 +0900

    r

---
+++ 08_Router/08_02_Router-basic/src/views/01_Router/PathVariable.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { onMounted, ref } from "vue";
+import { useRouter } from "vue-router";
+
+const route = useRouter();
+
+onMounted(()=>{
+    console.log(route.params);
+})
+</script>
+
+<template>
+    <h1>pathVariable : {{ $route.params.id }}</h1>
+</template>
